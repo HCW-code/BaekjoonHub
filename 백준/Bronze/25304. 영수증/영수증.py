942ddecdc195b43bde94dc29836641f8c1d3687d
홍찬우 <72719325+HCW-code@users.noreply.github.com>
2023-03-30 15:05:37 +0900

    [Bronze V] Title: 영수증, Time: 56 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.py
@@ -0,0 +1,15 @@
+import sys
+
+total = int(sys.stdin.readline().strip())
+count = int(sys.stdin.readline().strip())
+
+sum = 0
+
+for i in range(count):
+    a, b = map(int, sys.stdin.readline().strip().split())
+    sum += a*b
+
+if total == sum:
+    print('Yes')
+else:
+    print('No')
