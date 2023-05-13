8294c0cb11f25161fbf4c74aeb78556100383d3c
홍찬우 <72719325+HCW-code@users.noreply.github.com>
2023-05-13 19:16:17 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - String, Time: 157 ms, Memory: 61,180 KB -BaekjoonHub

--- SWEA/D3/1213. ［S／W 문제해결 기본］ 3일차 － String/［S／W 문제해결 기본］ 3일차 － String.py
+++ SWEA/D3/1213. ［S／W 문제해결 기본］ 3일차 － String/［S／W 문제해결 기본］ 3일차 － String.py
@@ -1,21 +1,10 @@
-for testcase in range(10):
+for _ in range(10):
     n = int(input())
     search = input()
     string = input()
     count = 0
     for st in range(len(string) - len(search)+1):
         if string[st] == search[0]:
-            flag = True
-            for se in range(len(search)):
-                if string[st+se] != search[se]:
-                    flag = False
-                    break
-            if flag:
-                count += 1
-                st += len(search) - 1
-    print(f'#{testcase+1} {count}')
-
-                
-        
-        
-
+            if string[st:st+len(search)] == search:
+                count+=1
+    print(f'#{n} {count}')
