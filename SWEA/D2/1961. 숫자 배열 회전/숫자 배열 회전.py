224a195116a63db8cd04d5bfff12e8894cde63da
홍찬우 <72719325+HCW-code@users.noreply.github.com>
2023-07-04 16:38:39 +0900

    [D2] Title: 숫자 배열 회전, Time: 153 ms, Memory: 58,244 KB -BaekjoonHub

--- SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.py
+++ SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.py
@@ -1,24 +1,63 @@
-test = int(input())
+import java.util.Scanner;
+import java.util.Arrays;
 
-for testcase in range(test):
-    n = int(input())
-    arr = []
-    result = [[0 for _ in range(n)] for _ in range(n)]
-    result1 = [[0 for _ in range(n)] for _ in range(n)]
-    result2 = [[0 for _ in range(n)] for _ in range(n)]
-    for _ in range(n):
-        arr.append(list(map(int, input().split())))
-    for i in range(n):
-        for j in range(n):
-            result[j][n-i-1] = arr[i][j]
-            result1[n-1-i][n-1-j] = arr[i][j]
-            result2[n-1-j][i] = arr[i][j]
-    print('#%d' %(testcase+1))
-    for i in range(n):
-        print(''.join(str(i) for i in result[i]), ''.join(str(i) for i in result1[i]), ''.join(str(i) for i in result2[i]))
+public class Solution {
 
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T = sc.nextInt();
+		for (int test_case = 1; test_case <= T; test_case++) {
+			int count = sc.nextInt();
+			int[][] arr = new int[count][count];
+			int[][] arr1 = new int[count][count];
+			int[][] arr2 = new int[count][count];
+			int[][] arr3 = new int[count][count];
+			for (int i = 0; i < count; i++) {
+				for (int j = 0; j < count; j++) {
+					arr[i][j] = sc.nextInt();
 
+				}
+			}
 
+			for (int i = 0; i < count; i++) {
+				for (int j = 0; j < count; j++) {
+					arr1[i][j] = arr[count - j - 1][i];
+				}
+			}
 
+			for (int i = 0; i < count; i++) {
+				for (int j = 0; j < count; j++) {
+					arr2[i][j] = arr1[count - j - 1][i];
 
+				}
+			}
 
+			for (int i = 0; i < count; i++) {
+				for (int j = 0; j < count; j++) {
+					arr3[i][j] = arr2[count - j - 1][i];
+
+				}
+			}
+			System.out.println("#"+test_case);
+
+			for (int i = 0; i < count; i++) {
+				for (int j = 0; j < count; j++) {
+					System.out.print(arr1[i][j]);
+				}
+				System.out.print(" ");
+				for (int j = 0; j < count; j++) {
+					System.out.print(arr2[i][j]);
+				}
+				System.out.print(" ");
+				for (int j = 0; j < count; j++) {
+					System.out.print(arr3[i][j]);
+				}
+				System.out.println("");
+			}
+
+		}
+		sc.close();
+	}
+
+}
