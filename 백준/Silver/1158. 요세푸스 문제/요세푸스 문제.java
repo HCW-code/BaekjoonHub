c342819ba803d74bcb6f28dca396c168a670208e
홍찬우 <72719325+HCW-code@users.noreply.github.com>
2023-07-18 23:19:48 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 696 ms, Memory: 296104 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+
+public class Main {
+
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int count = sc.nextInt();
+		int target = sc.nextInt();
+		int[] arr = new int[count];
+		
+		Queue<Integer> q = new LinkedList<>();
+		for(int i = 0; i < count; i++) {
+			q.add(i+1);
+		}
+		
+		int index = 0;
+		System.out.print("<");
+		while(q.size() != 0) {
+			for(int i = 0; i < target-1; i++) {
+				q.add(q.element());
+				q.remove();
+			}
+			arr[index] = q.element();
+			
+			if(q.size() == 1) {
+				System.out.print(q.element()+">");
+			}else {
+				System.out.print(q.element() + ", ");
+			}
+			
+			q.remove();
+			index++;
+		}
+		
+		
+		
+		
+		
+		
+	}
+}
